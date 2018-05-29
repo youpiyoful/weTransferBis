03456405e3450e20a05254ef264602bdac5fc608
youpiyoful <yoanfornari@gmail.com>
2018-05-29 09:33:49 +0200

    intermediaire.php + html

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+//  $url = explode('/', $_SERVER['REQUEST_URI'], 4);
+
+// // // j'ai supprimé la limite car le array_supprimait de toute façon le dernier élément même si c'était inférieur a 5
+//  if(count($url) > 3){
+//     array_pop($url);
+// }
+
+// //  var_dump($url);
+// // echo "<br>";
+// // var_dump($url);
+// // echo "<br>";
+// $path = implode('/', $url);
+
+
+$url = $_SERVER['REQUEST_URI'];
+
+
+switch ($url){
+case '/weTransferBis':
+case '/weTransferBis/':
+require_once('controllers/home.php');
+break;
+
+// case '/weTransferBis/intermediaire':
+// case '/weTransferBis/intermediaire/':
+// require_once('controllers/intermediaire.php');
+// break;
+
+
+case '/weTransferBis/reception':
+case '/weTransferBis/reception/':
+require_once('controllers/intermediaire.php');
+break;
+
+
+default:
+require_once('controllers/404-error.php');
+break;
+} 

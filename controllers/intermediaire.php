84ee68e9c860de5c2cdcbadadf6dd15b7d850760
youpiyoful <yoanfornari@gmail.com>
2018-06-01 13:49:47 +0200

    correction form + request modifié + intermediaire.php création d'une url de hachage + création d'une url page de téléchargement

--- controllers/intermediaire.php
+++ controllers/intermediaire.php
@@ -1,6 +1,7 @@
 <?php 
 
 require_once('models/request.php');
+require_once('utils/param.php');
 
 
 $mailExp = $_POST['expediteur'];
@@ -10,7 +11,8 @@ $fileName = $_FILES['upFile']['name'];
 $fileMessage = $_POST['message'];
 $fileSize = $_FILES['upFile']['size'];
 $fileType = $_FILES['upFile']["type"];
-// $fileLink
+$fileLink = $_SERVER['DOCUMENT_ROOT'].'/weTransferBis/assets/medias/files/'.$fileName;
+var_dump($_FILES);
 
 
 
@@ -25,10 +27,18 @@ $idExp = addEXpMail($mailExp);
 
 //fonction qui insère les infos à propos du fichier envoyé
 
-$idFile = insertDoc($fileName, $fileMessage, $fileSize);
+$idFile = insertDoc($fileName, $fileMessage, $fileSize, $fileLink);
 //fonction insérer le destinataire
 
 $idDest = addDestMail($mailDest);
+
+//fonction de hachage
+
+$codeUrl = hash('adler32', time());
+
+//url de la page de téléchargement
+
+$urlPageDl = "download/".$codeUrl;
 
 
 //fonction pour insérer tous les id propre au mail dans la table de liaison
@@ -43,29 +53,27 @@ if (isset($_FILES['upFile'])){
     move_uploaded_file($_FILES['upFile']['tmp_name'], $path);
 }
 
-// header("Location: reception");
 
 
 
-var_dump($_POST);
-$allInfos = linkAll($_GET['url']);
 
-var_dump($allInfos);
+// var_dump($_POST);
+// $allInfos = linkAll($_GET['url']);
 
-// envoie du mail au destinataire
+// var_dump($allInfos);
 
-if(isset($_POST['mail-dest'])){
-    echo("coucou");
-    $to = '$mailDest'; 
-    $subject = 'test-destinataire';
-    $message = 'blablabla';
-    $header = 'MIME version 1.0\r\n';
-    $header .= 'Content-type: text/html; charset=UTF-8\r\n';
-    mail($to,$subject,$message,$header);
-// envoie du mail a l'expéditeur
-} else if(isset($_POST['mail-exp'])){
-    mail($mailExp,'test-expediteur','blablabla');
-}
+// // envoie du mail au destinataire
 
-
+// if(isset($mailExp){
+//     $to = '$mailDest'; 
+//     $subject = 'test-destinataire';
+//     $message = 'blablabla';
+//     $header = 'MIME version 1.0\r\n';
+//     $header .= 'Content-type: text/html; charset=UTF-8\r\n';
+//     mail($to,$subject,$message,$header);
+// // envoie du mail a l'expéditeur
+// } else if(isset($_POST['mail-exp'])){
+//     mail($mailExp,'test-expediteur','blablabla');
+// }
 	
+// header("Location: reception");

83c39e88dad785db2a5de0cdeca19a683abcf9db
sirine <stagiaire@PORT0364>
2018-06-01 16:58:13 +0200

    mise à jour htaccess pour la route download

--- controllers/download.php
+++ controllers/download.php
@@ -5,3 +5,6 @@ require_once ('models/request.php');
 
 $loader = new Twig_Loader_Filesystem('./views');
 $twig = new Twig_Environment($loader, array('cache' => false));
+
+
+echo $twig->render('download.html');
--- index.php
+++ index.php
@@ -1,23 +1,17 @@
 <?php
 
-//  $url = explode('/', $_SERVER['REQUEST_URI'], 4);
+$url = explode('/', $_SERVER['REQUEST_URI'], 4);
 
-// // // j'ai supprimé la limite car le array_supprimait de toute façon le dernier élément même si c'était inférieur a 5
-//  if(count($url) > 3){
-//     array_pop($url);
-// }
+// j'ai supprimé la limite car le array_supprimait de toute façon le dernier élément même si c'était inférieur a 5
+if(count($url) > 3){
+   array_pop($url);
+ }
 
-// //  var_dump($url);
-// // echo "<br>";
-// // var_dump($url);
-// // echo "<br>";
-// $path = implode('/', $url);
+ var_dump($url);
 
+$path = implode('/', $url);
 
-$url = $_SERVER['REQUEST_URI'];
-
-
-switch ($url){
+switch ($path){
 case '/weTransferBis':
 case '/weTransferBis/':
 require_once('controllers/home.php');
@@ -42,4 +36,4 @@ break;
 default:
 require_once('controllers/404-error.php');
 break;
-} 
+}

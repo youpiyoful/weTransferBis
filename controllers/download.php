4ac38398aafc93fce038068957d339ef62bf9b74
sirine <stagiaire@PORT0364>
2018-06-01 16:25:53 +0200

    views download

--- controllers/download.php
+++ controllers/download.php
@@ -2,3 +2,6 @@
 
 require_once 'vendor/autoload.php';
 require_once ('models/request.php');
+
+$loader = new Twig_Loader_Filesystem('./views');
+$twig = new Twig_Environment($loader, array('cache' => false));

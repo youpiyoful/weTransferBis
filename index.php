24e991ed32313e3788c8d170045541d604f79c61
youpiyoful <yoanfornari@gmail.com>
2018-06-04 14:03:45 +0200

    remise en place du router pour le localhost

--- index.php
+++ index.php
@@ -2,45 +2,38 @@
 
  $url = explode('/', $_SERVER['REQUEST_URI'], 5);
 
-<<<<<<< HEAD
-// j'ai supprimé la limite car le array_supprimait de toute façon le dernier élément même si c'était inférieur a 5
-if(count($url) > 3){
-   array_pop($url);
- }
-
-=======
-// // j'ai supprimé la limite car le array_supprimait de toute façon le dernier élément même si c'était inférieur a 5
- if(count($url) > 4){
-    array_pop($url);
+//j'ai supprimé la limite car le array_supprimait de toute façon le dernier élément même si c'était inférieur a 5
+if(count($url) > 4){
+	array_pop($url);
 }
 
 //  var_dump($url);
 // echo "<br>";
 // var_dump($url);
 // echo "<br>";
->>>>>>> fa115ab4998459ba99496afe13d6b10115521dfa
+
 $path = implode('/', $url);
 var_dump($path);
 
 switch ($path){
-case '/projets/weTransferBis':
-case '/projets/weTransferBis/':
+case '/weTransferBis':
+case '/weTransferBis/':
 require_once('controllers/home.php');
 break;
 
-case '/projets/weTransferBis/intermediaire':
-case '/projets/weTransferBis/intermediaire/':
+case '/weTransferBis/intermediaire':
+case '/weTransferBis/intermediaire/':
 require_once('controllers/intermediaire.php');
 break;
 
 
-case '/projets/weTransferBis/reception':
-case '/projets/weTransferBis/reception/':
+case '/weTransferBis/reception':
+case '/weTransferBis/reception/':
 require_once('controllers/reception.php');
 break;
 
-case '/projets/weTransferBis/download':
-case '/projets/weTransferBis/download/':
+case '/weTransferBis/download':
+case '/weTransferBis/download/':
 require_once('controllers/download.php');
 break;
 

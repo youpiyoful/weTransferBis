a65bd972b230415076a94778da64434897cb05dd
Z-dorf <sebastian.zanon2@gmail.com>
2018-06-04 14:13:13 +0200

    merging back

--- index.php
+++ index.php
@@ -2,13 +2,6 @@
 
  $url = explode('/', $_SERVER['REQUEST_URI'], 5);
 
-<<<<<<< HEAD
-// j'ai supprimé la limite car le array_supprimait de toute façon le dernier élément même si c'était inférieur a 5
-if(count($url) > 3){
-   array_pop($url);
- }
-
-=======
 // // j'ai supprimé la limite car le array_supprimait de toute façon le dernier élément même si c'était inférieur a 5
  if(count($url) > 4){
     array_pop($url);
@@ -18,7 +11,6 @@ if(count($url) > 3){
 // echo "<br>";
 // var_dump($url);
 // echo "<br>";
->>>>>>> fa115ab4998459ba99496afe13d6b10115521dfa
 $path = implode('/', $url);
 var_dump($path);
 

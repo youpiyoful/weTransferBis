cb9f8662e5e33c23109b4a046e4796dea5a445ae
Z-dorf <sebastian.zanon2@gmail.com>
2018-06-05 10:31:31 +0200

    test fonction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+var inpF = document.querySelector('#inpfile');
+
+console.log(inpF);
+
 document.querySelector('#btnaddfile').addEventListener('click', function () {
-document.querySelector('#inpfile').click()
+inpF.click();
+// console.log('coucou');
 });
+
+function getfile() {
+    if  (inpF.style.display === "none") {
+        inpF.style.display = "block";
+    } else {  
+        console.log('fail');
+    }
+
+}

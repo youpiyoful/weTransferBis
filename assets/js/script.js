36502b5ddde76f50fbf163685a3faaba5d3671d9
Z-dorf <sebastian.zanon2@gmail.com>
2018-06-05 10:42:59 +0200

    alert function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,20 @@
 var inpF = document.querySelector('#inpfile');
-
-console.log(inpF);
+// console.log(inpF);
 
 document.querySelector('#btnaddfile').addEventListener('click', function () {
-inpF.click();
-// console.log('coucou');
+    inpF.click();
 });
 
-function getfile() {
-    if  (inpF.style.display === "none") {
-        inpF.style.display = "block";
-    } else {  
-        console.log('fail');
-    }
+// function getfile() {
+//     if  (inpF.style.display === "none") {
+//         inpF.style.display = "block";
+//     } else {  
+//         console.log('fail');
+//     }
 
-}
+// }
+
+$(inpF).change(function () {
+    var value = $(this).val();
+    alert(value);
+})
